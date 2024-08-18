6e2f4facffaa7f04124be24c38f517e2cd87e5ef
David Meskhishvili <dmeskh@gmail.com>
2024-08-17 22:50:24 -0700

    Finish styling data section

--- script.js
+++ script.js
@@ -11,3 +11,33 @@ const swiper = new Swiper(".swiper", {
     clickable: true,
   },
 });
+
+
+let menu = document.querySelector(".menu");
+let nums = document.querySelectorAll(".num");
+let start = false;
+
+window.addEventListener("scroll", () => {
+    const navbar = document.querySelector(".navbar");
+
+    navbar.classList.toggle("sticky", window.scrollY > 0);
+
+    if(window.scrollY >= menu.offsetTop) {
+        if(!start) {
+            nums.forEach(num => {
+                startCount(num);
+            });
+        };
+        start = true;
+    };
+});
+
+const startCount = (e) => {
+    let max = e.dataset.val;
+    let count = setInterval(() => {
+        e.textContent++;
+        if(e.textContent === max) {
+            clearInterval(count);
+        }
+    }, 2000 / nums);
+};
