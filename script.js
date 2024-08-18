40f0442545f8278225a0a45d256017e73ef721da
David Meskhishvili <dmeskh@gmail.com>
2024-08-18 01:00:24 -0700

    Make webpage responsive

--- script.js
+++ script.js
@@ -12,32 +12,31 @@ const swiper = new Swiper(".swiper", {
   },
 });
 
-
 let menu = document.querySelector(".menu");
 let nums = document.querySelectorAll(".num");
 let start = false;
 
 window.addEventListener("scroll", () => {
-    const navbar = document.querySelector(".navbar");
+  const navbar = document.querySelector(".navbar");
 
-    navbar.classList.toggle("sticky", window.scrollY > 0);
+  navbar.classList.toggle("sticky", window.scrollY > 0);
 
-    if(window.scrollY >= menu.offsetTop) {
-        if(!start) {
-            nums.forEach(num => {
-                startCount(num);
-            });
-        };
-        start = true;
-    };
+  if (window.scrollY >= menu.offsetTop) {
+    if (!start) {
+      nums.forEach((num) => {
+        startCount(num);
+      });
+    }
+    start = true;
+  }
 });
 
 const startCount = (e) => {
-    let max = e.dataset.val;
-    let count = setInterval(() => {
-        e.textContent++;
-        if(e.textContent === max) {
-            clearInterval(count);
-        }
-    }, 2000 / nums);
+  let max = e.dataset.val;
+  let count = setInterval(() => {
+    e.textContent++;
+    if (e.textContent === max) {
+      clearInterval(count);
+    }
+  }, 2000 / nums);
 };
